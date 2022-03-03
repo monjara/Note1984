e08338bda195cc04faa3b380c20f18f65fefec04
monjara <yaginumee@gmail.com>
2022-03-04 02:34:34 +0900

    et

--- src/screens/EditScreen.tsx
+++ src/screens/EditScreen.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import {Text} from 'react-native';
 
 const EditScreen = () => {
-  return <Text>'test'</Text>;
+  return <Text>'tsdfest'</Text>;
 };
 export default EditScreen;
